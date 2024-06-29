32a1c70461218f61aef806760ccea6d64f42f254
AnatolSalau <salovanatoly5@mail.ru>
2024-06-29 12:32:24 +0300

    Add comments

--- src/main/java/CrptApi.java
+++ src/main/java/CrptApi.java
@@ -104,7 +104,7 @@ public class CrptApi {
       }
 
       /**
-       * Replace "your_signature_token_here" with your actual bearer token.
+       * Replace "your_signature_token_here" with your actual signature token.
        */
       public static void main(String[] args) {
             String signature = "your_signature_token_here";
